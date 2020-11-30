df97db51b23f0460366ad629257ebcd5e47ff122
ararita <munjizavesna@gmail.com>
2020-11-30 16:11:49 +0100

    Set up done

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <section class="header">
+          <nav class="navbar">
+            <img src="images/ironhack-logo.svg" alt="logo" />
+            <img src="images/menu-top.svg" />
+          </nav>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
